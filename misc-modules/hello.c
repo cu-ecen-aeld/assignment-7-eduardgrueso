285ba10d33405409220d4c3611277bb683a52a78
eduard <egrueso@unal.edu.co>
2025-06-23 20:07:59 -0500

    initial changes

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -5,15 +5,17 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+char *author = "eduardgrueso";
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, %s\n", author);
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, %s\n", author);
 }
 
 module_init(hello_init);
